269f195029bfa83fc9a57f3e02f74e032c991977
ahmad alsharaira <a.alsharaira@alhisba.com>
2022-04-19 00:22:11 +0300

    create new project with first structure

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Products msg="WELCOME COMODO" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Products from "../modules/products/components/Products.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Products,
+  },
+};
+</script>
