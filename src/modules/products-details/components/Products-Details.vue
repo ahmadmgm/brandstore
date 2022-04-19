78cfe34d8971ed62908f4a13c76ad1b401d4fd11
ahmad alsharaira <a.alsharaira@alhisba.com>
2022-04-19 22:59:42 +0300

    added new module for products details and added all functionalaty

--- src/modules/products-details/components/Products-Details.vue
+++ src/modules/products-details/components/Products-Details.vue
@@ -1,65 +1,53 @@
-<template>
-  <div class="products-details" v-if="showpopup">
-    <div class="popup col-12 col-xs-12 col-sm-12 col-md-10 col-lg-6 col-xl-6">
-      <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="modal"
-        aria-label="Close"
-        v-on:click="closeDialog()"
-      ></button>
-      <h1 class="mt-4">Notify Me</h1>
-      <p v-if="productname">
-        Please enter your email address, we will contact you as soon as
-        <strong>{{ productname }}</strong> is back in stock
-      </p>
-      <input type="email" class="col-12 form-control" />
-      <button class="btn btn-dark col-12 mt-3">Notify Me</button>
-    </div>
-  </div>
-</template>
+<template src="../Product-Details.html"></template>
 
 <script>
 import { eventBus } from "../../../../src/main";
 export default {
   name: "ProductsDetails",
   data() {
-    return {};
+    return {
+      currentSizeIndex: 0,
+      regularPrice: "",
+      specialPrice: "",
+    };
   },
   computed: {},
   props: {
     showpopup: Boolean,
-    productname: String,
+    productData: [],
   },
   created() {},
   methods: {
+    calcPricePercentage(prodPrice) {
+      if (prodPrice.SAR.default_original == prodPrice.SAR.default) {
+        return 0;
+      }
+      const result = Math.round(
+        ((prodPrice.SAR.default_original - prodPrice.SAR.default) /
+          prodPrice.SAR.default_original) *
+          100
+      );
+      return result;
+    },
+
+    calcPricePercentageNewPrice(regularPrice, specialPrice) {
+      const result = Math.round(
+        ((regularPrice - specialPrice) / regularPrice) * 100
+      );
+      return result;
+    },
     closeDialog() {
-      eventBus.$emit("closeDialog", false);
+      eventBus.$emit("closeAddItemDialog", false);
+    },
+    selectIndexItem(index) {
+      this.currentSizeIndex = index;
+      let currentItemNewPriceIndex =
+        this.productData.associated_products[index];
+      this.regularPrice = currentItemNewPriceIndex.regular_price;
+      this.specialPrice = currentItemNewPriceIndex.special_price;
+      return this.currentSizeIndex;
     },
   },
 };
 </script>
-
-<style lang="scss">
-.popup {
-  background: #fff;
-  padding: 50px;
-  margin: 69px auto;
-  transition: 1s;
-}
-.products-details {
-  transition: 1s;
-  background: rgba(0, 0, 0, 0.6);
-  width: 100%;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-.btn-close {
-  float: right;
-  margin-top: 23px;
-}
-</style>
+<style src="../style/products-details.scss" lang="scss"></style>
--- src/modules/products/components/Products.vue
+++ src/modules/products/components/Products.vue
@@ -4,13 +4,16 @@
 import ProductsService from "../services/products.servies";
 import { eventBus } from "../../../../src/main";
 import PopupDialog from "../../popup-dialog/components/popup-dialog.vue";
+import ProductsDetails from "../../products-details/components/Products-Details.vue";
 export default {
   name: "Products",
   data() {
     return {
       productData: "",
       showPopup: false,
+      showPopupAddItem: false,
       productName: "",
+      singleProduct: "",
     };
   },
 
@@ -18,6 +21,9 @@ export default {
     this.getAllProducts();
     eventBus.$on("closeDialog", (data) => {
       this.showPopup = data;
+    });
+    eventBus.$on("closeAddItemDialog", (data) => {
+      this.showPopupAddItem = data;
     });
   },
   methods: {
@@ -46,6 +52,10 @@ export default {
         this.productData = prodData.data.hits;
       });
     },
+    showProductDialogPopup(product) {
+      this.showPopupAddItem = true;
+      this.singleProduct = product;
+    },
     showNotifyMePopup(productName) {
       this.showPopup = true;
       this.productName = productName;
@@ -53,6 +63,7 @@ export default {
   },
   components: {
     PopupDialog,
+    ProductsDetails,
   },
 };
 </script>
